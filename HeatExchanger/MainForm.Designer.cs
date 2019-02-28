06f382719c1bc9ffb35eb10b5d043bb30d56df8c
ProhorovaA <ya.prohor-an@ya.ru>
2019-02-28 15:06:07 +0400

    Первое добавление

---
+++ HeatExchanger/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mokrev_AA
+ * Date: 27.02.2019
+ * Time: 14:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace HeatExchanger
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(232, 151);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 31);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Вывод";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(232, 52);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 1;
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(56, 52);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(119, 23);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Название переменной для вывода";
+			this.label1.Click += new System.EventHandler(this.Label1_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(361, 260);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.button1);
+			this.Name = "MainForm";
+			this.Text = "HeatExchanger";
+			this.Load += new System.EventHandler(this.MainForm_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+		}
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+	}
+}
---
+++ HeatExchanger/MainForm.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Mokrev_AA
+ * Date: 27.02.2019
+ * Time: 14:19
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace HeatExchanger
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		
+		void Button1_Click(object sender, EventArgs e)
+		{
+			// TODO: Implement Button1_Click
+			
+			double Gm1 = 5;
+			double tnm1 = 20;
+			double tkm1 = 70;
+			double tmcp;
+			double Pizb1 = 1;
+		    double Patm = 1;
+		    double  Pabs;
+		    Pabs = Patm+Pizb1;
+		    Pabs = 2;
+		    
+		    
+		    
+		    double tkond = 119.6;
+		    
+		    double dtb;
+		    dtb = tkond - tnm1;
+		    dtb = 99.6;
+		    
+		    double dtm;
+		    dtm = tkond - tkm1;
+		     dtm =49.6;
+		     
+		    double dtcp;
+		     dtcp = (dtb - dtm)/Math.Log(dtb/dtm);
+		     dtcp = 71.8;
+		      tmcp  = tkond - dtcp;
+		      tmcp = 47.8;
+		      
+		    double cm = 1787;
+		    double Q;
+		       Q = Gm1*cm*(tnm1-tkm1);
+		       Q = 469200;
+		       
+		    double  r = 2208*10^3;
+		    double Gnapa = Q/r;
+		    Gnapa = 0.215;	     
+		   
+		    int Kopient = 300;
+		    
+		    double Fopient;
+		    Fopient = Q/(Kopient*dtcp);
+		}
+		
+	}
+}
